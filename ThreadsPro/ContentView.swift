79d4e91cf26da0047162c4ef15af0054f76bedf9
Abhijith C <abhijithchalilabc@gmail.com>
2025-03-29 23:37:44 +0530

    Initial Commit

---
+++ ThreadsPro/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ThreadsPro
+//
+//  Created by Abhijith Chalil on 29/03/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ThreadsProTests/ThreadsProTests.swift
@@ -0,0 +1,17 @@
+//
+//  ThreadsProTests.swift
+//  ThreadsProTests
+//
+//  Created by Abhijith Chalil on 29/03/25.
+//
+
+import Testing
+@testable import ThreadsPro
+
+struct ThreadsProTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
